4d21ea9aff915a651bf920b959ce604b43343ecd
theKwabena <abbeyemmanueltetteh@gmail.com>
2023-06-20 10:37:06 +0000

    new

--- gbdamis/authentication/urls.py
+++ gbdamis/authentication/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('sign-up', views.sign_up_view, name='signup'),
     path('send', views.send_verification_email, name='send-verification-email'),
     path('verify-email', views.verification_sent, name='verification'),
-    path('verify-user/<uid64>/<token>/<email>/', views.verify_user, name='verify-user')
+    path('verify-user/<uid64>/<token>/', views.verify_user, name='verify-user')
 ]
